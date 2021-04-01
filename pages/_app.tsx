2189faa52abf8cea7b803c480863518e095c2df1
giaunguyen <giau.nguyen@executionlab.asia>
2021-04-01 17:28:49 +0700

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
 
+import React, { useEffect } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    window.addEventListener('load', () => {
+      const body = document.getElementsByTagName("body")[0] as HTMLBodyElement;
+      if (body)
+        body.classList.add("loaded")
+    })
+  })
 
   return (
    <Component {...pageProps} />
